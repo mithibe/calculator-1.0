f0e9ac48a72a74c9fd841e8bdc86ca12f732bd15
Mithibe Irungu <Kenmithibe@gmail.com>
2023-02-08 08:56:03 +0300

    re-arranged the number buttons

---
+++ js/calculator.js
@@ -0,0 +1,11 @@
+var screen = document.getElementById("dislplayScreen");
+var numpad = document.getElementById("numpad");
+var buttons = document.getElementsByTagName("button")
+
+function write() {
+    screen = buttons.innerHTML;
+}
+
+buttons.write()
+
+
